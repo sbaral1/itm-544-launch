0d748a8c96340d9894f56775cde40f703b6c3cba
sbaral1 <sbaral1@hawk..it.edu>
2015-10-27 15:40:36 -0500

    modified the script

--- launch.sh
+++ launch.sh
@@ -1,5 +1,5 @@
 aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --key-name $4 --security-group-ids $5 --subnet-id $6
---associate-public-ip-address --user-data file://install-webserver.sh --debug
+--associate-public-ip-address --user-data file://install-webserver.sh --iam-profile $7--debug
 
 echo ami image-id $1
 
@@ -12,3 +12,5 @@ echo security-group-ids $5
 echo subnet $6
 
 echo key-name $4
+
+echo iam-profile $7
